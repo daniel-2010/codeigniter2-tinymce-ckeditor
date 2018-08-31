4af699619ccc264f27dd13a431191e5ffa009dd7
Daniel <42868748+daniel-2010@users.noreply.github.com>
2018-08-31 14:17:42 -0300

    Add files via upload

--- banco/db_codeigniter.sql
+++ banco/db_codeigniter.sql
@@ -1,21 +1,36 @@
 -- --------------------------------------------------------
 -- Servidor:                     127.0.0.1
--- Versão do servidor:           5.6.20 - MySQL Community Server (GPL)
+-- Versão do servidor:           5.5.27 - MySQL Community Server (GPL)
 -- OS do Servidor:               Win32
--- HeidiSQL Versão:              8.3.0.4694
+-- HeidiSQL Versão:              9.1.0.4867
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Copiando estrutura do banco de dados para db_codeigniter
-CREATE DATABASE IF NOT EXISTS `db_codeigniter` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `db_codeigniter`;
+-- Copiando estrutura para tabela db_codeigniter.cidade
+DROP TABLE IF EXISTS `cidade`;
+CREATE TABLE IF NOT EXISTS `cidade` (
+  `idCidade` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_cid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idCidade`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela db_codeigniter.cidade: ~5 rows (aproximadamente)
+/*!40000 ALTER TABLE `cidade` DISABLE KEYS */;
+INSERT INTO `cidade` (`idCidade`, `nome_cid`) VALUES
+	(1, 'Belo Horizonte'),
+	(2, 'São Paulo'),
+	(3, 'Rio de Janeiro'),
+	(4, 'Curitiba'),
+	(5, 'Maceió');
+/*!40000 ALTER TABLE `cidade` ENABLE KEYS */;
 
 
 -- Copiando estrutura para tabela db_codeigniter.usuario
+DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE IF NOT EXISTS `usuario` (
   `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
@@ -25,15 +40,17 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `senha` varchar(45) NOT NULL,
   `status` int(11) NOT NULL,
   `nivel` int(11) NOT NULL,
+  `funcao` int(11) DEFAULT NULL,
+  `descricao_funcao` varchar(50) DEFAULT NULL,
+  `descricao` text,
   PRIMARY KEY (`idUsuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela db_codeigniter.usuario: ~1 rows (aproximadamente)
-DELETE FROM `usuario`;
+-- Copiando dados para a tabela db_codeigniter.usuario: ~2 rows (aproximadamente)
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` (`idUsuario`, `nome`, `cpf`, `endereco`, `email`, `senha`, `status`, `nivel`) VALUES
-	(3, 'Derpilho', '123.123.123-23', 'R. Example, 012', 'derpilho@gmail.com', 'aa47f8215c6f30a0dcdb2a36a9f4168e', 1, 1),
-	(4, 'Derp D Boas', '123.123.123-23', 'R. Example, 012', 'derpilho@gmail.com', '912ec803b2ce49e4a541068d495ab570', 1, 1);
+INSERT INTO `usuario` (`idUsuario`, `nome`, `cpf`, `endereco`, `email`, `senha`, `status`, `nivel`, `funcao`, `descricao_funcao`, `descricao`) VALUES
+	(3, 'Daniel User', '123.123.123-23', 'R. Example, 012', 'email@exemplo.com', 'aa47f8215c6f30a0dcdb2a36a9f4168e', 1, 1, NULL, NULL, NULL),
+	(5, 'Daniel User 2', '00000000', 'Rua teste', 'email@exemplo.com', 'aa47f8215c6f30a0dcdb2a36a9f4168e', 1, 1, 1, 'Apenas administrador', '<h2 style="text-align: left;"><span class="marker"><em><strong>Descri&ccedil;&atilde;o do usuario</strong></em></span></h2>\r\n<p>&nbsp;</p>\r\n<p style="text-align: left;">&nbsp;</p>');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
